a06b6a9a533f60af428253dfdb136ac72389c9ff
Jessie Liang <rnliang.jessie@gmail.com>
2026-04-09 23:23:44 -0700

    step 5 done

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
-# interactive app
-
+# import libraries
 import pandas as pd
 import numpy as np
 from rank_bm25 import BM25Okapi
@@ -13,8 +12,16 @@ import faiss
 import dash
 from dash import dcc, html, Input, Output, State, Dash
 import dash_bootstrap_components as dbc
+import os
+import logging
+from transformers import logging as hf_logging
 
+# suppress warnings
+os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
+hf_logging.set_verbosity_error()
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
+# read and clean data
 meta = pd.read_json("data/raw/meta_Toys_and_Games.jsonl", lines = True, nrows=10000)
 review = pd.read_json("data/raw/Toys_and_Games.jsonl", lines = True, nrows=10000)
 
@@ -41,7 +48,9 @@ cleaned_meta['categories'] = cleaned_meta['categories'].apply(
 
 # combining all the important and useful columns from the meta dataset
 products = (
-    cleaned_meta['title'].fillna('') + ' ' +
+    "TITLE: " + cleaned_meta['title'].fillna('') + " | " +
+    "RATING: " + cleaned_meta['average_rating'].fillna(0).astype(str) + " | " +
+    "TEXT: " +
     cleaned_meta['description'].fillna('') + ' ' +
     cleaned_meta['features'].fillna('') + ' ' +
     cleaned_meta['categories'].fillna('')
@@ -126,9 +135,30 @@ def retrieve(n_clicks, n_submit, query, mode):
         return dbc.Alert("Please enter a search query.", color="warning")
  
     results = bm25_search(query) if mode == "bm25" else embedding_search(query)
+    reviews = []
+    for rank, (product, score) in enumerate(results, start=1):
+        title = re.search(r"TITLE:\s*(.*?)\s*\|", product)
+        title = title.group(1) if title else ""
+        rating = re.search(r"RATING:\s*(.*?)\s*\|", product)
+        rating = rating.group(1) if rating else ""
+        text = re.search(r"TEXT:\s*(.*)", product)
+        text = text.group(1) if text else ""
+        
+        reviews.extend([
+            f"Product title = {title}",
+            html.Br(),
+            f"Average rating = {rating}",
+            html.Br(),
+            f"Retrieval Score = {score:.3f}",
+            html.Br(),
+            f"Text: {text[:200]}......",
+            html.Br(),
+            html.Br(),
+            html.Br()
+        ])
 
     cards = dbc.Card([
-        results
+        dbc.CardBody(reviews)
     ])
  
     header = html.P(
